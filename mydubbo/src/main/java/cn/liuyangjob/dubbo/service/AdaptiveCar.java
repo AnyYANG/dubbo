c380ed8937db9ebd42836e1895fc700102d7cb59
liuyang <me@liuyangjob.cn>
2019-11-22 17:50:28 +0800

    liuyang
    自适应

--- mydubbo/src/main/java/cn/liuyangjob/dubbo/service/AdaptiveCar.java
+++ mydubbo/src/main/java/cn/liuyangjob/dubbo/service/AdaptiveCar.java
@@ -1,24 +1,25 @@
 package cn.liuyangjob.dubbo.service;
 
 import cn.liuyangjob.dubbo.api.car;
-import com.alibaba.dubbo.common.Extension;
 import com.alibaba.dubbo.common.URL;
+import com.alibaba.dubbo.common.extension.Adaptive;
 
 /**
  * Created by  liuyang
- * 2019/11/22    14:50
+ * 2019/11/22    17:25
  * cn.liuyangjob.dubbo.service
  * All Right Reserved by liuyang.
  **/
-public class BlackCar  implements car {
+@Adaptive
+public class AdaptiveCar implements car {
+
     @Override
     public void getColor() {
-        System.out.println("BLACK");
+        System.out.println("adaptiveCar");
     }
 
     @Override
     public void getColorUrl(URL url) {
-
+        System.out.println("hahahahahah");
     }
-
 }
